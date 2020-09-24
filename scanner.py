9c4be8d5de26ebb36fbb6749faba3d607e8c4edc
Wil McKean <wilmckean92@gmail.com>
2020-09-24 15:52:39 -0400

    added processor information

--- scanner.py
+++ scanner.py
@@ -1,32 +1,35 @@
 import socket, re, uuid
-from sys import platform
+import sys, os
+import platform
 from netifaces import (
     interfaces,
     ifaddresses,
     AF_INET
 )
-
 # TODO: Add Linux and OSX support
 
 class GetHostInformation():
-    def get_localhost_information(hostname):
+    def get_localhost_information(hostname=socket.gethostname()):
         '''
-        Scan Local host for basic information
+        Returns a dictionary of host information.
         '''
-        # TODO: Add IPv6
+        operating_system = platform.platform(True, True)
+        processor = platform.machine()
+
+        sys_info = platform.uname()
 
         ipv4_addresses = []
-        operating_system = platform
         fqdn = socket.getfqdn()
         mac = ":".join(re.findall('..', '%012x' % uuid.getnode()))
 
         for interface in interfaces():
                 for inet in ifaddresses(interface).get(AF_INET, ()):
                     ipv4_addresses.append(inet['addr'])
-
+        
         host_data = {
             "hostname": hostname,
             "operating_system": operating_system,
+            "processor": processor,
             "fqdn": fqdn,
             "mac": mac,
             "ip_address_ipv4": ipv4_addresses[0],
