e74b4001e48c08b4e3218834c8bde68dddb5b381
Wil McKean <wilmckean92@gmail.com>
2020-09-24 15:55:01 -0400

    Added speedtest_avg and refactored

--- network_diagnostics.py
+++ network_diagnostics.py
@@ -2,8 +2,9 @@
 Checks basic network functionality. 
 '''
 from subprocess import Popen
-import speedtest
+import speedtest as st
 import socket
+from decimal import Decimal
 
 class NetworkDiagnostics():
 
@@ -11,7 +12,6 @@ class NetworkDiagnostics():
         try:
             socket.setdefaulttimeout(timeout)
             socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((connectTo, port))
-            print("T")
             return True
         except socket.error as err:
             print(err)
@@ -19,9 +19,8 @@ class NetworkDiagnostics():
 
     def ping_check(addressToPing='8.8.8.8'):
         '''
-        Check to see if the destination is reachable.
+        Checks to see if a destination is reachable.
         '''
-       
         ping = Popen(f'ping {addressToPing}')
         ping.wait()
 
@@ -35,9 +34,8 @@ class NetworkDiagnostics():
     # Add traceroute for Linux
     def nslookup_check(siteToCheck='https://google.com'):
         '''
-        Check to see if DNS is resolving.
+        Checks to see if DNS is resolving.
         '''
-
         ns = Popen(f'nslookup {siteToCheck}')
         ns.wait()
 
@@ -48,31 +46,47 @@ class NetworkDiagnostics():
             print("DNS works")
             return True
 
-    def speedtest(timesToRun=1):
+    def speedtest():
         '''
         Returns download[0], upload[1] and ping[2] rates in the form of a list.
         '''
         servers = []
         threads = None
 
-        s = speedtest.Speedtest()
+        s = st.Speedtest()
         s.get_servers(servers)
         s.get_best_server()
 
-        for i in range(timesToRun):
+        s.download(threads=threads)
+        s.upload(threads=threads)
+        s.results.share()
 
-            s.download(threads=threads)
-            s.upload(threads=threads)
-            s.results.share()
+        results = [s.results.download, s.results.upload, s.results.ping]
 
-            keys = ['download', 'upload', 'ping']
-            results = [s.results.dict().get(k,{}) for k in keys]
+        # Formats the objects in the results list to have 2 decimal places.
+        # Also converts download and upload from bits to Mbits
+        for val in range(len(results)):
+            if val == 2:
+                results[val] = Decimal(str(results[val])).quantize(Decimal('1.00'))
+                continue
+            results[val] = results[val] / (1024 ** 2)
+            results[val] = Decimal(str(results[val])).quantize(Decimal('1.00'))
+        return(results)
+    
+    def speedtest_avgs(runs=5):
 
-            for val in range(len(results)):
-                if val == 2:
-                    results[val] = float("{:.2f}".format(results[val]))
-                    continue
-                results[val] = results[val] / (1024 ** 2)
-                results[val] = float("{:.2f}".format(results[val]))
-                
-            return(results)
+        download = Decimal('0.0')
+        upload = Decimal('0.0')
+        ping = Decimal('0.0')
+        
+        for run in range(runs):
+            temp_list = NetworkDiagnostics.speedtest()
+            download += Decimal(str(temp_list[0]))
+            upload += Decimal(str(temp_list[1]))
+            ping += Decimal(str(temp_list[2]))
+
+        avg_dl = Decimal(str(download/runs)).quantize(Decimal('1.00'))
+        avg_up = Decimal(str(upload/runs)).quantize(Decimal('1.00'))
+        avg_ping = Decimal(str(ping/runs)).quantize(Decimal('1.00'))
+
+        return str([avg_dl, avg_up, avg_ping])
